2bb053c3d95cdae53a0c958bb4ed2be5c5d57b37
RANINO <raninvlz@gmail.com>
2021-06-17 12:08:06 +0700

    hi

---
+++ app/src/main/java/com/informatika/projectandroid/MainLayout.kt
@@ -0,0 +1,11 @@
+package com.informatika.projectandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_layout)
+    }
+}
